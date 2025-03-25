3c5eb6fc100dcca25fad55a91f28a33960cf5963
Chaitanya306 <98307330+Chaitanya306@users.noreply.github.com>
2025-03-25 16:35:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-	let arr=str.split('')
-	for(let a of arr){
-		if(arr.count(a)==1) return a
+	//let arr=str.split('')
+	for(let a of str){
+		if(str.indexOf(a)==str.lastIndexOf(a)){
+			return a
+		}
 	}
 	return null
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+//const input = prompt("Enter a string");
+//alert(firstNonRepeatedChar(input)); 
